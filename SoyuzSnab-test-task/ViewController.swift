c01350e445e1f392786f70afbf58ab4d155ed0ac
ofigensky <bulletproofgleb@icloud.com>
2022-10-06 16:42:19 +0300

    Initial Commit

---
+++ SoyuzSnab-test-task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoyuzSnab-test-task
+//
+//  Created by Gleb Glushok on 06.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
